5002d117e7652814d3bd3e8a0360e0e9a3d8f393
Zach Hopkins <hopkinsza@gmail.com>
2026-02-15 11:55:37 -0500

    add option parsing to redshift.sh

--- bin/redshift.sh
+++ bin/redshift.sh
@@ -1,1 +1,35 @@
-redshift -m randr -l '41.08:-85.14'
+#!/bin/sh
+if ps axo comm | grep -qx redshift; then
+	echo 'redshift already running' >&2
+	exit 1
+fi
+
+bright=0.6
+temp=4200
+
+while getopts 123b:t: f; do
+	case $f in
+	1)
+		bright=0.8
+		temp=5000
+		;;
+	2)
+		bright=0.7
+		temp=4500
+		;;
+	3)
+		bright=0.6
+		temp=4200
+		;;
+	b)
+		bright=$OPTARG
+		;;
+	t)
+		temp=$OPTARG
+		;;
+	esac
+done
+shift $(( $OPTIND - 1 ))
+
+# ft wayne: 41.08:-85.14
+redshift -r -m randr -l '28.989:-82.686' -t ${temp}K:${temp}K -b ${bright}:${bright}
